b1c3ac4a350e378f654926d94d750e06c27467c6
Shinichi <shinichi.yoshinaga@brother.co.jp>
2024-11-25 12:56:02 +0900

    up

--- 2-kaggle-recommendation-exp/functions/data_operator.py
+++ 2-kaggle-recommendation-exp/functions/data_operator.py
@@ -1,11 +1,13 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class DataOperator:
     def __init__(self, file_path):
         print("initializing DataOperator")
         self.__read_csv(file_path)
+        self.__clean_df()
 
     def __read_csv(self, file_path):
         print("reading csv file: " + file_path)
@@ -16,3 +18,24 @@ class DataOperator:
         else:
             self.df = pd.read_csv(file_path)
 
+    def __clean_df(self):
+        self.df = self.df.dropna()
+
+    def display_items(self):
+        # ratingのヒストグラム
+        data_rating = self.df.groupby("Rating").count()
+        print(data_rating)
+        plt.subplot(2, 2, 1)
+        plt.bar(data_rating.index, data_rating["UserId"])
+        plt.xlabel("Rating")
+        plt.ylabel("Count")
+
+        plt.subplot(2, 2, 2)
+        data_eval = self.df.groupby("UserId")[["Rating"]].mean()
+        print(data_eval)
+        data_eval.sort_values("Rating", ascending=False, inplace=True)
+        data_eval = data_eval.head(10)
+        
+        plt.xticks(rotation=90)
+        plt.bar(data_eval.index, data_eval["Rating"])
+        plt.show()
--- 2-kaggle-recommendation-exp/main.py
+++ 2-kaggle-recommendation-exp/main.py
@@ -1,16 +1,17 @@
 
 
 import os, sys
-
 import functions
+
 
 def main():
     print("executing main.py")
     dir_current = os.path.dirname(os.path.abspath(__file__))
     data_operator = functions.DataOperator(f"{dir_current}/data/ratings_Beauty.csv")
+    data_operator.display_items()
 
 
 if __name__ == '__main__':
     print("executing main.py")
-    data_operator = functions.DataOperator()
+    main()
 
