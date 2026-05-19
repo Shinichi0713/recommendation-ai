e056541169b07cfad71ad707000ff5b6d6855c0b
Shinichi Yoshinaga <yoshishinnze@gmail.com>
2026-05-20 05:58:16 +0900

    up

--- src/replace.py
+++ src/replace.py
@@ -64,8 +64,8 @@ def format_to_note_style(content, output_file):
     print(f"変換が完了しました！\n出力先: {output_file}")
 
 # 実行設定
-input_md = r"D:\PycharmProjects\ScienceCalculation\idea_thinking\optimization\basic\l2_convex.md"   # 変換元のファイル名
-output_md = r"D:\PycharmProjects\ScienceCalculation\idea_thinking\optimization\basic\l2_convex.md" # 変換後のファイル名
+input_md = r"D:\PycharmProjects\ScienceCalculation\recommendation-ai\mathematics\linear_algebra\docs\21_svd.md"   # 変換元のファイル名
+output_md = r"D:\PycharmProjects\ScienceCalculation\recommendation-ai\mathematics\linear_algebra\docs\21_svd.md" # 変換後のファイル名
 is_note = False
 
 
